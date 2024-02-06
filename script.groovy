2d4a0ca65fd4e7d0da8f2358430c7f94faceed2f
Syed Mujtahid Bin Tawhid <mujtahid2018@gmail.com>
2024-02-06 19:51:09 +0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t mujtahid_tawhid/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push mujtahid_tawhid/demo-app:jma-2.0'
     }
 } 
 
